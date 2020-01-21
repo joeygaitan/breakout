e03e426dce3c6cd6c4ff5debb68dc8625f67c7a6
Joey Gaitan <joeygaitan707@gmail.com>
2020-01-21 11:39:38 -0800

    added in logical structure to build levels of blocks for later. I finished page 2 as well'

--- game.js
+++ game.js
@@ -4,31 +4,38 @@ let boxScript = document.querySelector('.boxScript');
 let valueBoxScript = boxScript.getContext('2d');
 
 valueBoxScript.beginPath();
-valueBoxScript.rect(100, 50, 120, 56);
+valueBoxScript.rect(20, 40, 50, 50);
 valueBoxScript.fillStyle = "#0095DD";
 valueBoxScript.fill();
+valueBoxScript.closePath();
 
-for(let i =1;i<=12;i++){
+valueBoxScript.beginPath();
+valueBoxScript.arc(240, 160, 20, 0, Math.PI*2, false);
+valueBoxScript.fillStyle = "green";
+valueBoxScript.fill();
+valueBoxScript.closePath();
 
-    
+valueBoxScript.beginPath();
+valueBoxScript.rect(160, 10, 100, 40);
+valueBoxScript.strokeStyle = "rgba(0, 0, 255, 0.5)";
+valueBoxScript.stroke();
+valueBoxScript.closePath();
+
+// for(let i =1;i<=12;i++){
+//     const x = (100 * i % 3) + 30
+//     const y = 100 * Math.floor(100 / i)
 
 
-    const x = (100 * i % 3) + 30
-    const y = 100 * Math.floor(100 / i)
-    
-    document.getElementsByTagName("canvas")[i - 1].setAttribute("class", "rectangle" + i);
 
-    valueAnotherBox.beginPath();
+//     // if(i>=4){
 
-    if(i>=4){
+//     // }else if(i>4 && i<=8){
 
-    }else if(i>4 && i<=8){
+//     // }else if(i>8 && i<=12){
 
-    }else if(i>8 && i<=12){
+//     // }
+// }
 
-    }
-    document.querySelector('#blocks').appendChild(boxScript)
-}
 //responsive web formula
 //1. listen for changes in window size
 //2. update size of canvas
