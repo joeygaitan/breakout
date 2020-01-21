a99a65d45e19eb7aabaad8beec07c5fd0db31162
Joey Gaitan <joeygaitan707@gmail.com>
2020-01-21 11:53:57 -0800

    got the ball to move in a random direction

--- game.js
+++ game.js
@@ -1,25 +1,47 @@
 
+
 let boxScript = document.querySelector('.boxScript');
 
 let valueBoxScript = boxScript.getContext('2d');
 
-valueBoxScript.beginPath();
-valueBoxScript.rect(20, 40, 50, 50);
-valueBoxScript.fillStyle = "#0095DD";
-valueBoxScript.fill();
-valueBoxScript.closePath();
+// valueBoxScript.beginPath();
+// valueBoxScript.rect(20, 40, 50, 50);
+// valueBoxScript.fillStyle = "#0095DD";
+// valueBoxScript.fill();
+// valueBoxScript.closePath();
 
-valueBoxScript.beginPath();
-valueBoxScript.arc(240, 160, 20, 0, Math.PI*2, false);
-valueBoxScript.fillStyle = "green";
-valueBoxScript.fill();
-valueBoxScript.closePath();
+// valueBoxScript.beginPath();
+// valueBoxScript.arc(240, 160, 20, 0, Math.PI*2, false);
+// valueBoxScript.fillStyle = "green";
+// valueBoxScript.fill();
+// valueBoxScript.closePath();
 
-valueBoxScript.beginPath();
-valueBoxScript.rect(160, 10, 100, 40);
-valueBoxScript.strokeStyle = "rgba(0, 0, 255, 0.5)";
-valueBoxScript.stroke();
-valueBoxScript.closePath();
+// valueBoxScript.beginPath();
+// valueBoxScript.rect(160, 10, 100, 40);
+// valueBoxScript.strokeStyle = "rgba(0, 0, 255, 0.5)";
+// valueBoxScript.stroke();
+// valueBoxScript.closePath();
+
+var x = boxScript.width/2;
+var y = boxScript.height-30;
+var dx = 2;
+var dy = -2;
+
+const drawBall = () =>{
+    valueBoxScript.beginPath();
+    valueBoxScript.arc(x, y, 10, 0, Math.PI*2);
+    valueBoxScript.fillStyle = "#0095DD";
+    valueBoxScript.fill();
+    valueBoxScript.closePath();
+}
+
+const draw = () => {
+    valueBoxScript.clearRect(0, 0, boxScript.width, boxScript.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
 
 // for(let i =1;i<=12;i++){
 //     const x = (100 * i % 3) + 30
