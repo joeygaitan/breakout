58f23e928ef42941fe6a12e71e034dea206f3c51
Joey Gaitan <joeygaitan707@gmail.com>
2020-01-22 15:04:07 -0800

    added in assets image. Planned pong game

--- game.js
+++ game.js
@@ -28,12 +28,48 @@ var dx = 2;
 var dy = -2;
 var ballRadius = 10;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (boxScript.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+
 const drawBall = () =>{
     valueBoxScript.beginPath();
     valueBoxScript.arc(x, y, ballRadius, 0, Math.PI*2);
     valueBoxScript.fillStyle = "#0095DD";
     valueBoxScript.fill();
     valueBoxScript.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
 }
 
 const draw = () => {
