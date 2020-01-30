9a97f093c66b9852ead30d09b988dbf400bb5632
Joey Gaitan <joeygaitan707@gmail.com>
2020-01-30 10:05:21 -0800

    built file structure

--- classIndex.js
+++ classIndex.js
@@ -1,13 +1,13 @@
 class Ball{
     constructor(radius = 10, color = 'red') {
       this.color = color
+      this.ballRadius = radius
       this.x = 0
       this.y = 0
       this.dx = 2
       this.dy = 2
-      this.ballRadius = radius
     }
-  
+    
     render = (ctx) => {
       const color = this.color
   
@@ -16,6 +16,10 @@ class Ball{
       ctx.fillStyle = color;
       ctx.fill();
       ctx.closePath();
+    }
+
+    move = () => {
+
     }
   }
 
@@ -27,57 +31,29 @@ class Brick {
 
 class Paddle {
     constructor(){
-        
+
+    }
+}
+
+class Background {
+    constructor(){
+
     }
 }
   
+class Score {
+    constructor(){
+
+    }
+}
+
+class Lives {
+    constructor(){
+
+    }
+}
   class Game {
-    constructor(idTag){
-  
-      this.canvas = document.getElementById(idTag),
-      this.ctx = getContext('2d')
-  
-      this.level = 0
-      this.lives = 3
-      this.score = {current:0,total:0}
-  
-      this.ball = new Ball('red')
-      this.ball.render(this.ctx)
-      
-      this.ball = {
-        ballRadius: 10,
-        x: this.canvas.width / 2,
-        y: this.canvas.height - 30,
-        dx: 2,
-        dy: -2
-      }
-  
-      this.paddle = {
-        paddleHeight: 10,
-        paddleWidth: 75,
-        paddleX: (this.canvas.width - paddleWidth) / 2
-      }
-  
-  
+    constructor(){
+
     }
-  
-    ballMovement = () =>{
-      if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        this.ball.dx = -this.ball.dx;
-      }
-      if (y + dy < ballRadius) {
-        dy = -dy;
-      } else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
-          if (ballRadius >= 1.5) {
-            ballRadius -= 1.5;
-          }
-          dy = -dy;
-        }
-      }
-    }
-  
-    drawGame = () =>{
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-  }
+}
